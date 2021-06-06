a8d390b80abd29ef57e3a29deffb02197a13db92
Jose <josetsaji2006@gmail.com>
2021-06-06 13:00:30 +0530

    added minimum price rule to first backer card

--- script.js
+++ script.js
@@ -19,11 +19,39 @@ const backprojectClickElems = [btnBackProject, $(".slct-reward1"),
     $(".slct-reward2")];
 const radioCheckClickElems = [radioInput.slice(0, 3)];
 
-$(".input")[0].checked = false;
-$("body").onunload =
+var input1 = $(".input-price1");
+var input2 = $(".input-price2");
+var input3 = $(".input-price3");
+
+var con1 = $(".price-continue")[0];
+var con2 = $(".price-continue")[1];
+var con3 = $(".price-continue")[2];
+
+var input1Div = $(".pledgeWrapper1");
+var input2Div = $(".pledgeWrapper2");
+var input3Div = $(".pledgeWrapper3");
+
+con1.disabled = true;
+con2.disabled = true;
+con3.disabled = true;
+
+// $(".input")[0].checked = false;
+$(window).onload = function () {
     html.animate({
         scrollTop: 0
     }, "slow");
+}
+
+$("form").on("submit", function () {
+    return false;
+});
+
+if ($(window).width() < 480 == true) {
+    $(".icn-ham").show();
+    $(".hero-img-desktop").removeAttr("src");
+    $(".hero-img-desktop").attr("src", "/images/image-hero-mobile.jpg");
+};
+
 $(".input")[1].checked = false;
 $(".input")[2].checked = false;
 $(".pledgeSection").hide();
@@ -34,7 +62,7 @@ backToTopBtn.on("click", function () {
 })
 $(window).scroll(function () {
     if (html.scrollTop() > 840) {
-        backToTopBtn.fadeIn(600);
+        backToTopBtn.fadeIn(600).css("display", "flex");
     } else {
         backToTopBtn.fadeOut(400)
     }
@@ -46,54 +74,67 @@ radioCheckClickElems.forEach(function (elem) {
         if (timesClicked > 1) {
             $("input").prop("checked", false)
             timesClicked = 0;
-        }
-        if (radioInput[0].checked == true) {
+
+            $(".icn-close").on("click", function () {
+                if (timesClicked == 1) {
+                    timesClicked = 0;
+                    $(".backProjectCard1 hr").hide();
+                    $(".backProjectCard2 hr").hide()
+                    $(".backProjectCard3 hr").hide()
+                }
+            })
+
+        };
+        if (elem[0].checked == true) {
             if ($(window).width() < 480) {
                 $(".backProjectCard1").addClass("heightIncrease");
             };
             $(".backProjectCard1 .pledgeSection").slideDown();
             $(".backProjectCard1 hr").show();
-            radioInput[0].parentElement.style.border = "2px solid hsl(176, 50%, 47%)";
-        } else if (radioInput[0].checked == false) {
+            input1.focus();
+            elem[0].parentElement.style.border = "2px solid hsl(176, 50%, 47%)";
+        } else if (elem[0].checked == false) {
             $(".backProjectCard1").removeClass("heightIncrease");
             setTimeout(function () {
                 $(".backProjectCard1 hr").hide();
             }, 115)
             $(".backProjectCard1 .pledgeSection").slideUp(500);
-            radioInput[0].parentElement.style.border = "2px solid #ececec";
+            elem[0].parentElement.style.border = "1px solid #d9d9d9";
 
         };
 
-        if (radioInput[1].checked == true) {
+        if (elem[1].checked == true) {
             if ($(window).width() < 480) {
                 $(".backProjectCard2").addClass("heightIncrease");
             }
             $(".backProjectCard2 .pledgeSection").slideDown();
             $(".backProjectCard2 hr").show();
-            radioInput[1].parentElement.style.border = "2px solid hsl(176, 50%, 47%)";
-        } else if (radioInput[1].checked == false) {
+            input2.focus();
+            elem[1].parentElement.style.border = "2px solid hsl(176, 50%, 47%)";
+        } else if (elem[1].checked == false) {
             $(".backProjectCard2").removeClass("heightIncrease");
             setTimeout(function () {
                 $(".backProjectCard2 hr").hide();
             }, 115)
             $(".backProjectCard2 .pledgeSection").slideUp(500);
-            radioInput[1].parentElement.style.border = "2px solid #ececec";
+            elem[1].parentElement.style.border = "1px solid #d9d9d9";
         };
 
-        if (radioInput[2].checked == true) {
+        if (elem[2].checked == true) {
             if ($(window).width() < 480) {
                 $(".backProjectCard3").addClass("heightIncrease");
             }
             $(".backProjectCard3 .pledgeSection").slideDown();
             $(".backProjectCard3 hr").show();
-            radioInput[2].parentElement.style.border = "2px solid hsl(176, 50%, 47%)";
-        } else if (radioInput[2].checked == false) {
+            input3.focus();
+            elem[2].parentElement.style.border = "2px solid hsl(176, 50%, 47%)";
+        } else if (elem[2].checked == false) {
             $(".backProjectCard3").removeClass("heightIncrease");
             setTimeout(function () {
                 $(".backProjectCard3 hr").hide();
             }, 115)
             $(".backProjectCard3 .pledgeSection").slideUp(500);
-            radioInput[2].parentElement.style.border = "2px solid #ececec";
+            elem[2].parentElement.style.border = "1px solid #d9d9d9";
         };
     })
 })
@@ -101,13 +142,11 @@ radioCheckClickElems.forEach(function (elem) {
 backprojectClickElems.forEach(function (elem) {
     elem.on("click", function () {
         hamburger.hide();
-        $(".backProject").fadeIn(500);
+        $(".backProject").fadeIn(300);
         $(".mn-cd-1").hide();
         $(".mn-cd-2").hide();
         $(".mn-cd-3").hide();
-        html.animate({
-            scrollTop: 0
-        }, "slow")
+        html.scrollTop(0);
     });
 });
 function bkmrkChange() {
@@ -135,6 +174,7 @@ function bkmrkDefault() {
 }
 
 $(".icn-close").click(function () {
+    $(".backProjectCard hr").hide();
     hamburger.show();
     $(".backProject").fadeOut(300);
     $(".mn-cd-1").show();
@@ -144,18 +184,10 @@ $(".icn-close").click(function () {
     $(".input")[1].checked = false;
     $(".input")[2].checked = false;
     backProjectCard.removeClass("heightIncrease");
-    backProjectCard.css("border", "2px solid #ececec");
+    backProjectCard.css("border", "1px solid #d9d9d9");
     $(".pledgeSection").hide();
-    html.animate({
-        scrollTop: 0
-    }, "slow");
+    html.scrollTop(0);
 });
-
-function changeColorBorder() {
-    $("#card-h").hover(function () {
-        $(".checkmark").css("border", "solid #3cb4ac 2px;");
-    });
-};
 
 btnContinue.click(function () {
     $(".backProject").hide();
@@ -172,20 +204,18 @@ btnContinue.click(function () {
     $(".input")[0].checked = false;
     $(".input")[1].checked = false;
     $(".input")[2].checked = false;
+    $(".input-price")[0].value = "";
+    input2.val("");
+    input3.val("");
     $("hr").hide();
     backProjectCard.removeClass("heightIncrease");
-    backProjectCard.css("border", "2px solid #ececec");
+    backProjectCard.css("border", "1px solid #d9d9d9");
     $(".pledgeSection").hide();
     if (window.matchMedia("(max-width: 400px)").matches) {
-        html.animate({
-            scrollTop: 80
-        }, "slow");
+        html.scrollTop(80)
     };
     if (window.matchMedia("(min-width: 500px)").matches && window.matchMedia("(max-width: 1400px)").matches) {
-        html.animate({
-            scrollTop: 260
-        }, "slow");
-        body.css("overflow", "hidden");
+        html.scrollTop(260);
     };
 });
 
@@ -197,55 +227,148 @@ $(".gotIt").click(function () {
     $(".hero-img-desktop").css("filter", "contrast(100%)")
     $(".thanks").hide();
     body.css("overflow", "auto");
-    html.animate({
-        scrollTop: 0
-    }, "slow");
+    html.scrollTop(0);
+});
+
+//For Card 1
+input1.keydown(function () {
+    if (input1.val() < 0) {
+        input1Div.css("pointer-events", "none")
+        input1Div.css("border", "1px solid #83afad")
+        con1.style.backgroundColor = "#83afad";
+        input1.css("pointer-events", "auto");
+        con1.disabled = true;
+    } else if (input1.val() > 0) {
+        con1.disabled = false;
+        input1Div.css("pointer-events", "auto");
+        input1Div.css("border", "1px solid hsl(176, 50%, 47%)")
+        con1.style.backgroundColor = null;
+    };
 })
 
-//Inputs
-var input2 = $(".input-price2");
-var input3 = $(".input-price3");
+input1.focus(function () {
+    if (input1.val() < 1) {
+        input1Div.css("pointer-events", "none")
+        input1Div.css("border", "1px solid #83afad")
+        con1.style.backgroundColor = "#83afad";
+        input1.css("pointer-events", "auto");
+        con1.disabled = true;
+    } else if (input1.val() > 0) {
+        con1.disabled = false;
+        input1Div.css("pointer-events", "auto");
+        input1Div.css("border", "1px solid hsl(176, 50%, 47%)")
+        con1.style.backgroundColor = null;
+    };
+})
 
-//CONTINUE BUTTONS
-var con2 = $(".card2Btn");
-var con3 = $(".card3Btn");
+input1.keyup(function () {
+    if (input1.val() < 0) {
+        input1Div.css("pointer-events", "none")
+        input1Div.css("border", "1px solid #83afad")
+        con1.style.backgroundColor = "#83afad";
+        input1.css("pointer-events", "auto");
+        con1.disabled = true;
+    } else if (input1.val() > 0) {
+        con1.disabled = false;
+        input1Div.css("pointer-events", "auto");
+        input1Div.css("border", "1px solid hsl(176, 50%, 47%)")
+        con1.style.backgroundColor = null;
+    };
+})
 
-var btn2 = $(".btn-2");
 
 
 //For Card 2
-
-function minPledge2() {
+input2.keydown(function () {
     if (input2.val() < 25) {
-        btn2.css("border", "1px solid #83afad");
-        con2.css("pointer-events", "none")
-        con2.css("background-color", "#83afad")
+        input2Div.css("pointer-events", "none")
+        input2Div.css("border", "1px solid #83afad")
+        con2.style.backgroundColor = "#83afad";
+        input2.css("pointer-events", "auto");
     } else if (input2.val() > 25 || input2.val() == 25) {
-        btn2.css("border", "1px solid hsl(176, 50%, 47%)");
-        con2.css("pointer-events", "auto")
-        con2.css("background-color", "hsl(176, 50%, 47%)")
-    }
-};
+        con2.disabled = false;
+        input2Div.css("pointer-events", "auto")
+        input2Div.css("border", "1px solid hsl(176, 50%, 47%)")
+        con2.style.backgroundColor = null;
+    };
+})
+
+input2.focus(function () {
+    if (input2.val() < 25) {
+        input2Div.css("pointer-events", "none")
+        input2Div.css("border", "1px solid #83afad")
+        con2.style.backgroundColor = "#83afad";
+        input2.css("pointer-events", "auto");
+        con2.disabled = true;
+    } else if (input2.val() > 25 || input2.val() == 25) {
+        input2Div.css("pointer-events", "auto")
+        input2Div.css("border", "1px solid hsl(176, 50%, 47%)")
+        con2.style.backgroundColor = null;
+        con2.disabled = false;
+    };
+})
+
+input2.keyup(function () {
+    if (input2.val() < 25) {
+        input2Div.css("pointer-events", "none")
+        input2Div.css("border", "1px solid #83afad");
+        con2.style.backgroundColor = "#83afad";
+        input2.css("pointer-events", "auto");
+        con2.disabled = true;
+    } else if (input2.val() > 25 || input2.val() == 25) {
+        input2Div.css("pointer-events", "auto");
+        input2Div.css("border", "1px solid hsl(176, 50%, 47%)");
+        con2.style.backgroundColor = null;
+        con2.disabled = false;
+    };
+})
 
 //For Card 3
 
-function minPledge3() {
+input3.keydown(function () {
     if (input3.val() < 75) {
-        btn2.css("border", "1px solid #83afad");
-        con3.css("pointer-events", "none")
-        con3.css("background-color", "#83afad")
+        input3Div.css("pointer-events", "none")
+        input3Div.css("border", "1px solid #83afad");
+        con3.style.backgroundColor = "#83afad";
+        con3.disabled = true;
+        input3.css("pointer-events", "auto")
     } else if (input3.val() > 75 || input3.val() == 75) {
-        btn2.css("border", "1px solid hsl(176, 50%, 47%)");
-        con3.css("background-color", "hsl(176, 50%, 47%)")
-        con3.css("pointer-events", "auto")
-    }
-};
+        input3Div.css("pointer-events", "auto")
+        input3Div.css("border", "1px solid hsl(176, 50%, 47%)");
+        con3.style.backgroundColor = null;
+        con3.disabled = false
+    };
+});
 
-if ($(window).width() < 480) {
-    $(".icn-ham").show();
-    $(".hero-img-desktop").removeAttr("src");
-    $(".hero-img-desktop").attr("src", "/images/image-hero-mobile.jpg");
-}
+input3.focus(function () {
+    if (input3.val() < 25) {
+        input3Div.css("pointer-events", "none")
+        input3Div.css("border", "1px solid #83afad")
+        con3.style.backgroundColor = "#83afad";
+        con3.disabled = true;
+        input3.css("pointer-events", "auto");
+    } else if (input3.val() > 75 || input3.val() == 75) {
+        input3Div.css("pointer-events", "auto")
+        input3Div.css("border", "1px solid hsl(176, 50%, 47%)")
+        con3.style.backgroundColor = null;
+        con3.disabled = false;
+    };
+})
+
+input3.keyup(function () {
+    if (input3.val() < 75) {
+        input3Div.css("pointer-events", "none")
+        input3Div.css("border", "1px solid #83afad");
+        con3.style.backgroundColor = "#83afad";
+        con3.disabled = true;
+        input3.css("pointer-events", "auto")
+    } else if (input3.val() > 75 || input3.val() == 75) {
+        input3Div.css("pointer-events", "auto")
+        input3Div.css("border", "1px solid hsl(176, 50%, 47%)");
+        con3.style.backgroundColor = null;
+        con3.disabled = false
+    };
+});
 
 hamburger.click(function () {
     navbarMini.show();
@@ -266,7 +389,7 @@ iconClose.click(function () {
         top: "15px"
     }, 250);
     setTimeout(function () {
-        navbarMini.fadeOut(100);  
+        navbarMini.fadeOut(100);
     }, 100)
     $(this).hide()
     hamburger.show();
